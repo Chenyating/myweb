8596eaa58d68aa97ccdc28aa5a2f74a90443dd80
yating <924471303@qq.com>
2019-08-17 18:05:38 +0800

    修改一下文章列表

--- pages/mobile/articleList.vue
+++ pages/mobile/articleList.vue
@@ -1,20 +1,50 @@
 <template>
 <div>
-    <nuxt-link to="/">首页</nuxt-link>
-    这是列表
+    <nuxt-link to="/mobile">返回</nuxt-link>
+    <div v-for="(item,index) in blogList" :key="index">
+        <Card class="card-box">
+            <div class="title">{{item.title}}</div>
+            <div class="gray-text">{{item.date}}</div>
+        </Card>
+    </div>
 </div>
 </template>
 <script>
 export default {
-    methods: {
-        getList() {
+    data() {
+        return {
+            blogList: [{
+                    title: '这是博客的名称',
+                    date: '2019-10-12 18:50',
+                },
+                {
+                    title: '这是博客的名称',
+                    date: '2019-10-12 18:50',
+                },
+                {
+                    title: '这是博客的名称',
+                    date: '2019-10-12 18:50',
+                },
+                {
+                    title: '这是博客的名称',
+                    date: '2019-10-12 18:50',
+                },
+            ]
         }
+    },
+    methods: {
+        getList() {}
     },
     mounted() {
         this.getList();
     }
 }
 </script>
-<style lang="less" scoped>
-
+<style lang="less">
+@import "~assets/css/mobile/base.less";
+.card-box {
+    margin: @distansBig;
+    padding: @distansBig;
+    .border-shadow();
+}
 </style>
