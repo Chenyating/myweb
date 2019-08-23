5f2ea1e7db91b8e477a3b990528f2ef6e6085ff9
yating <924471303@qq.com>
2019-08-23 17:54:28 +0800

    修改首页

--- pages/mobile/index/index.vue
+++ pages/mobile/index/index.vue
@@ -1,84 +1,104 @@
 <template>
 <!-- mobile首页 -->
 <div class="mobile-index-box">
-    <div class="flex">
+    <div class="flex" v-for="(item,index) in list" :key="index">
         <img class="head-img" src="~static/mobile/index/yating.jpg" />
         <div class="shuoshuo">
             <div class="title">用户名</div>
-            <div class="text">本人性格开朗、稳重、有活力，待人热情、真诚；工作认真负责，积极主动</div>
-            <img class="content-img" src="~static/mobile/index/yating.jpg"/>
+            <div class="text">{{item.content}}</div>
+            <!-- 视频 -->
+            <video v-if="item.type==3" class="content-img" width="320" height="240" controls="controls" autoplay="autoplay"><source :src="item.url" type="video/mp4" /></video>
+            <img v-if="item.type==2" class="content-img" :src="item.url" />
             <div class="gray-text">2019-11-13 11:20</div>
         </div>
     </div>
-    <div class="flex">
-        <img class="head-img" src="~static/mobile/index/yating.jpg" />
-        <div class="shuoshuo">
-            <div class="title">用户名</div>
-            <div class="text">本人性格开朗、稳重、有活力，待人热情、真诚；工作认真负责，积极主动</div>
-            <div class="gray-text">2019-11-13 11:20</div>
-        </div>
-    </div>
-    <div class="flex">
-        <img class="head-img" src="~static/mobile/index/yating.jpg" />
-        <div class="shuoshuo">
-            <div class="title">用户名</div>
-            <div class="text">本人性格开朗、稳重、有活力，待人热情、真诚；工作认真负责，积极主动</div>
-            <img class="content-img" src="~static/mobile/index/yating.jpg"/>
-            <div class="gray-text">2019-11-13 11:20</div>
-        </div>
-    </div>
-    <div class="flex">
-        <img class="head-img" src="~static/mobile/index/yating.jpg" />
-        <div class="shuoshuo">
-            <div class="title">用户名</div>
-            <div class="text">本人性格开朗、稳重、有活力，待人热情、真诚；工作认真负责，积极主动</div>
-            <img class="content-img" src="~static/mobile/index/yating.jpg"/>
-            <div class="gray-text">2019-11-13 11:20</div>
-        </div>
+    <div @click="getmore">
+        <Divider orientation="center" class="text">{{ifmore?'点击更多……':"已经到底啦"}}</Divider>
     </div>
 </div>
 </template>
 <script>
-import outerRouter from '~/components/public/outerRouter'
+import outerRouter from '~/components/public/outerRouter';
+import SERVER from '~/assets/server/api.js';
+
 export default {
     components: {
         outerRouter
     },
     data() {
         return {
-            name: "YATING",
-            me: {
-                name: "YATING",
-                intro: "用心写代码，不辜负程序员之名"
-            }
+            list: null,
+            page: 0,
+            num: 2,
+            ifmore: true
         };
+    },
+    methods: {
+        getshuoshuo(page, num) {
+            var params = {
+                page: page,
+                num: num
+            }
+            SERVER.getshuoshuo(params)
+                .then((data) => {
+                    // 返回的长度为0则表示，没有更多了。
+                    if (data.data.length == 0) {
+                        this.ifmore = false;
+                    } else {
+                        // 如果当前页码为0，那么就重新赋值给留言列表；
+                        if (this.page == 0) {
+                            this.list = data.data;
+                        } else {
+                            // 否则就在留言列表后面继续添加
+                            this.list = this.list.concat(data.data);
+                        }
+                    }
+                })
+                .catch(err => {});
+        },
+        // 获得更多；
+        getmore() {
+            if (this.ifmore) {
+                this.page = this.page + 1;
+                this.getshuoshuo(this.num, this.page);
+            } else {
+                return;
+            }
+        }
+    },
+    mounted() {
+        this.getshuoshuo(this.page, this.num);
     }
 };
 </script>
 <style lang="less" scoped>
 @import "~assets/css/mobile/base.less";
-.flex{
+.flex {
     flex-wrap: nowrap;
     margin: @distansBig;
     padding: @distansBig 0;
     border-bottom: @line-sizeSmall @line-color solid;
 }
-.title{
+
+.title {
     line-height: 16px;
 }
-.text{
+
+.text {
     line-height: 24px;
 }
+
 .mobile-index-box {
     .head-img {
+        @smallWidth: 20%;
         .icon(@width: 20%);
         padding: 0 @distansBig;
     }
-    .shuoshuo{
+    .shuoshuo {
         width: 80%;
     }
-    .content-img{
-        width: 50%;
+    .content-img {
+        width: 80%;
         height: auto;
         margin: @distansSmall 0;
     }
