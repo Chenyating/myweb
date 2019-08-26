21145798a4c8ae3a2ef6150ef161fc6fe378c783
yating1 <yating1@qq.com>
2019-08-27 00:37:32 +0800

    添加jq动画

--- layouts/mobile/index.vue
+++ layouts/mobile/index.vue
@@ -1,68 +1,95 @@
 <template>
-<div class="D-show">
+  <div class="D-show">
     <div id="box"></div>
     <Header></Header>
-    <nuxt/>
+    <nuxt />
     <Footer :num="times"></Footer>
-</div>
+  </div>
 </template>
 <script>
-import SERVER from '~/assets/server/api.js'
-import Header from '~/components/mobile/header';
-import Footer from '~/components/mobile/footer'
+import SERVER from "~/assets/server/api.js";
+import Header from "~/components/mobile/header";
+import Footer from "~/components/mobile/footer";
 export default {
-    components: {
-        Header,
-        Footer
+  components: {
+    Header,
+    Footer
+  },
+  data() {
+    return {
+      times: null,
+      imgUrl: require("~/static/mobile/index/me.png"),
+      haha:null
+    };
+  },
+  methods: {
+    getRandom(x, y) {
+      return Math.floor(Math.random() * (y - x)) + x;
     },
-    data() {
-        return {
-            times: null,
-            imgUrl: require("~/static/mobile/index/me.png")
-        }
-    },
-    methods: {
-        playFloatBox() {
-            var getbox = $("#box");
-            for (let index = 0; index < 10; index++) {
-                getbox.append(` <img class="icon" src="${this.imgUrl}"/>`);
-            }
-            $("#box img").animate({
-                opacity: '1',
-                height: '40px',
-                width: '40px',
-                rotate:'180deg',
-            });
-        }
-    },
-    mounted() {
-        var params = {
-            name: "mobileIndex"
-        }
-        SERVER.addTimes(params).then((data) => {
-            // console.log(data)
-        }).catch((err) => {
-            console.log(err)
-        })
-        SERVER.getTimes(params).then((data) => {
-            this.times = data.data[0].times;
-            console.log(this.times)
-        }).catch((err) => {
-            console.log(err)
-        })
-        this.playFloatBox();
+    playFloatBox() {
+      var getbox = $("#box");
+      for (let index = 0; index < 10; index++) {
+        var item = $(` <img class="icon" src="${this.imgUrl}"/>`);
+        item.css({
+          "position": "fixed",
+          "top": `${this.getRandom(0, 100)}%`,
+          "right": `${this.getRandom(0, 100)}%`,
+          "transfrom": "rotate(180deg)",
+          "animation": "rotateLong 2s infinite;nim",
+          "width":`${this.getRandom(50, 80)}px`,
+          "height":"auto",
+          "opacity": "0.7",
+        });
+        item.animate({
+            opacity: "1",
+            width:"0",
+            top:`${this.getRandom(0, 100)}%`
+          },10000);
+        getbox.append(item);
+      }
     }
-}
+  },
+  mounted() {
+    var params = {
+      name: "mobileIndex"
+    };
+    SERVER.addTimes(params)
+      .then(data => {
+        // console.log(data)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    SERVER.getTimes(params)
+      .then(data => {
+        this.times = data.data[0].times;
+        console.log(this.times);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    //   this.haha=setInterval(()=>{
+    //       this.playFloatBox();
+    //   },5000)
+  },
+  destroyed(){
+      clearInterval(this.haha)
+      console.log("xia")
+  }
+};
 </script>
 <style lang="less" scoped>
 @import "~assets/css/mobile/base.less";
 #box {
-    position: absolute;
-    z-index: 9999999;
+  position: fixed;
+  z-index: 9999999;
 }
 
 .icon {
-    .icon(@width: 40px);
+  .icon(@width: 40px);
+}
+.D-show {
+  position: relative;
 }
 </style>
 
