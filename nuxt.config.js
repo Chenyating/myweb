3777025082dde9024ed7ed810054c95e52997406
yating <924471303@qq.com>
2019-08-27 17:28:03 +0800

    完善好项目列表

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'myweb',
+    title: `yating's world`,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
