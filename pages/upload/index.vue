7d99f9e34af0743b62811f1eb3fccd401d3dcc20
yating <924471303@qq.com>
2019-08-29 18:14:36 +0800

    新增上传代码

---
+++ pages/upload/index.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <Upload action="/api/upload/img" multiple type="drag">
+        <Button icon="ios-cloud-upload-outline">Upload files</Button>
+    </Upload>
+</div>
+</template>
+<script>
+export default {
+
+}
+</script>
