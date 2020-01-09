0de21e32c30f64fdddcb84155b66f4fb8d1d961e
sushe <924471303@qq.com>
2020-01-09 20:54:26 +0800

    modify

--- layouts/error.vue
+++ layouts/error.vue
@@ -6,16 +6,16 @@
         </h1>
         <h1 v-else>500 - 我想给的给不了</h1>
     </div>
-    <canvas id="stage" :width="canvasWidth" :height="canvasHeight"></canvas>
+    <!-- <canvas id="stage" :width="canvasWidth" :height="canvasHeight"></canvas> -->
 </div>
 </template>
 
 <script>
-import {
-    mother,
-    getMouseXY,
-    myname
-} from "../pages/demo/ball";
+// import {
+//     mother,
+//     getMouseXY,
+//     myname
+// } from "../pages/demo/ball";
 
 export default {
     data() {
@@ -26,34 +26,34 @@ export default {
             balls: []
         };
     },
-    methods: {
-        begin(num) {
-            for (let i = 0; i < num; i++) {
-                var qiu = new mother(this.canvasWidth, this.canvasHeight, this.ctx);
-                qiu.init();
-                qiu.draw();
-                this.balls.push(qiu);
-            }
-            setInterval(() => {
-                this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-                for (const item of this.balls) {
-                    item.move();
-                }
-            }, 1000 / 60);
-        }
-    },
-    mounted() {
-        this.canvasWidth = Math.floor(document.body.clientWidth);
-        this.canvasHeight = Math.ceil(document.body.clientHeight);
-        window.onresize = () => {
-            this.canvasWidth = Math.floor(document.body.clientWidth);
-            this.canvasHeight = Math.ceil(document.body.clientHeight);
-            // console.log(this.canvasWidth, this.canvasHeight)
-        };
-        var c = document.getElementById("stage");
-        this.ctx = c.getContext("2d");
-        this.begin(100);
-    },
+    // methods: {
+    //     begin(num) {
+    //         for (let i = 0; i < num; i++) {
+    //             var qiu = new mother(this.canvasWidth, this.canvasHeight, this.ctx);
+    //             qiu.init();
+    //             qiu.draw();
+    //             this.balls.push(qiu);
+    //         }
+    //         setInterval(() => {
+    //             this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    //             for (const item of this.balls) {
+    //                 item.move();
+    //             }
+    //         }, 1000 / 60);
+    //     }
+    // },
+    // mounted() {
+    //     this.canvasWidth = Math.floor(document.body.clientWidth);
+    //     this.canvasHeight = Math.ceil(document.body.clientHeight);
+    //     window.onresize = () => {
+    //         this.canvasWidth = Math.floor(document.body.clientWidth);
+    //         this.canvasHeight = Math.ceil(document.body.clientHeight);
+    //         // console.log(this.canvasWidth, this.canvasHeight)
+    //     };
+    //     var c = document.getElementById("stage");
+    //     this.ctx = c.getContext("2d");
+    //     this.begin(100);
+    // },
     props: ["error"]
 };
 </script>
